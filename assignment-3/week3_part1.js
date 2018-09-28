00428da71c028f8d470c69805653c389e80da22b
miopio <mioakasako@gmail.com>
2018-09-28 11:30:04 -0400

    Create week3_part1.js

---
+++ assignment-3/week3_part1.js
@@ -0,0 +1,35 @@
+var request = require('request'); // npm install request
+var async = require('async'); // npm install async
+var fs = require('fs');
+
+var apiKey = process.env.TAMU_KEY; 
+console.log(apiKey);
+
+var meetingsData = []; //hold results of request
+
+//bring in organized addresses from week2
+var addresses = JSON.parse(fs.readFileSync('/home/ec2-user/environment/data/week2/address2.txt'));
+
+// eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
+    apiRequest += 'streetAddress=' + value.split(' ').join('%20');
+    apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
+    apiRequest += '&format=json&version=4.01';
+    
+    request(apiRequest, function(err, resp, body) {
+        if (err) {throw err;}
+        else {
+            var tamuGeo = JSON.parse(body);
+            console.log(tamuGeo['FeatureMatchingResultType']);
+            meetingsData.push(tamuGeo);
+        }
+    });
+    setTimeout(callback, 2000);
+}, function() {
+    fs.writeFileSync('data/week3/meetingsData2.json', JSON.stringify(meetingsData));
+    console.log('*** *** *** *** ***');
+    console.log('Number of meetings in this zone: ');
+    console.log(meetingsData.length);
+    console.log(meetingsData);
+});
